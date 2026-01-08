42b35f24ca6971cfc2b87d7d6e805dfaa505b1b9
Amanda Voigt <amanda02_vg@hotmail.com>
2026-01-07 21:57:12 -0300

    Applying Listener and Functions

--- siteada/script.js
+++ siteada/script.js
@@ -1,6 +1,10 @@
-if (document.getElementById("nome").value !== "" &&
-    document.getElementById("email").value !== "") {
-    alert("Printinho! você receberá as novidades por email.");
-} else {
-    alert("Por favor, preencha os campos nome e e-mail.");
+document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
+
+function validaFormulario() {
+    if (document.getElementById("nome").value !== "" &&
+        document.getElementById("email").value !== "") {
+        alert("Printinho! você receberá as novidades por email.");
+    } else {
+        alert("Por favor, preencha os campos nome e e-mail.");
+    }
 }
